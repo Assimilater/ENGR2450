c988cec016c58fa4a5b20de94eef66f1e63fe5fe
John Call <johnkc1992@gmail.com>
2015-02-22 04:17:57 -0700

    Implement some of Gauss elimination

--- src/assign3/assign3.cpp
+++ src/assign3/assign3.cpp
@@ -1,18 +1,65 @@
 #include "assign3.h"
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
-#include "../shared/matrix.h"
 using namespace assign3;
+
+std::vector<double> assign3::TryGauss(
+    Matrix<double> &a,
+    std::vector<double>b,
+    double tolerance,
+    GaussError& error)
+{
+    if (a.Rows != a.Cols != b.size()) {
+        error = GaussError::INV_DIM;
+        return b;
+    }
+
+    std::vector<double> x, s;
+    double temp;
+    for (int i = 0; i < a.Rows; ++i) {
+        s[i] = 0;
+        for (int j = 0; j < a.Cols; ++j) {
+            temp = abs(a[i][j]);
+            if (temp > s[i]) {
+                s[i] = temp;
+            }
+        }
+    }
+
+    return x;
+}
+
+/*
+1 2  2       4
+1 2  1       
+1 2
+
+*/
 
 void assign3::Problem1() {
     Matrix<double> a{
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
+        {1, 2, -1},
+        {5, 2, 2},
+        {-3, 5, -1},
     };
+    std::vector<double> b{2, 9, 1};
+    std::vector<double> x;
+    GaussError error = GaussError::GOOD;
 
-    std::cout << a[2][1] << std::endl;
+    x = TryGauss(a, b, .001, error);
+
+    // Output the result of Gaussian eleminiation with partial pivoting
+    if (error == GaussError::GOOD) {
+        std::cout << "The result is: " << x << std::endl;
+    } else if (error == GaussError::INV_DIM) {
+        std::cout << "The matrix a and the solution vector b have invalid dimensions..." << std::endl;
+    } else if (error == GaussError::SINGULAR) {
+        std::cout << "The matrix a is singular, and ax = b has no solution..." << std::endl;
+    } else {
+        std::cout << "An unkown error has occured..." << std::endl;
+    }
 }
 
 void assign3::main() {
--- src/assign3/assign3.h
+++ src/assign3/assign3.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <vector>
+#include "../shared/matrix.h"
 
 namespace assign3 {
-    void Problem1();
+    enum GaussError {
+        GOOD = 0,
+        INV_DIM = 1,
+        SINGULAR = 2
+    };
 
+    std::vector<double> TryGauss(Matrix<double>&, std::vector<double>, double, GaussError&);
+
+    void Problem1();
     void main();
 }
