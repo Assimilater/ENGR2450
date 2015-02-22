84061b8433c093f6e94958c770a0620bfe0dd458
John Call <johnkc1992@gmail.com>
2015-02-21 23:27:28 -0700

    Copy split method from previous courses

--- src/assign3/assign3.cpp
+++ src/assign3/assign3.cpp
@@ -8,9 +8,11 @@ using namespace assign3;
 void assign3::Problem1() {
     Matrix<double> a{
         {1, 2, 3},
-        {1, 2, 3},
-        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
     };
+
+    std::cout << a[2][1] << std::endl;
 }
 
 void assign3::main() {
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,10 @@
 #include "assign1\assign1.h"
 #include "assign2\assign2.h"
 #include "assign3\assign3.h"
-#include "shared\matrix.h"
 #include <iostream>
 
 int main() {
     assign3::main();
-
-    Matrix<double> a{{1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
-    std::cout << a[1][1] << std::endl;
 
     system("pause");
     return 0;
