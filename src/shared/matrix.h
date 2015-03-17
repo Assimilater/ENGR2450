cbe97d839bd978e744172a1658beaf06d1ecc947
John Call <johnkc1992@gmail.com>
2015-03-16 18:39:40 -0600

    Cleanup

--- src/shared/matrix.h
+++ src/shared/matrix.h
@@ -9,7 +9,6 @@ template <typename T>
 class Matrix {
 private:
     T** _array = nullptr;
-    static T _default;
 
 public:
     // Important properties
@@ -17,14 +16,13 @@ public:
 
     bool isSingular() { return _array == nullptr; };
 
-    static void setDefault(T val) { _default = val; };
-
     // public access to _array
     T* operator [](int r) { return _array[r]; };
 
-    // Common operations (implemented externally
+    // Common operations (implemented externally)
     Matrix<T> Inverse(bool&) const;
     Matrix<T> Transpose() const;
+    T Determinant(bool&) const;
     T Trace(bool&) const;
 
     // Allow for C++11 initializer_list
@@ -47,11 +45,10 @@ public:
         }
     };
 
-    // Override default constructors, operator=, and destructor
-    Matrix(int m, int n) { init(m, n, _default); };
-    Matrix(int m, int n, T val) { init(m, n, val); };
-    Matrix<T>& operator=(const Matrix<T> &a) { copy(a); return *this; };
-    Matrix(const Matrix<T> &a) { copy(a); }
+    // Implement/Override constructors, operator=, and destructor
+    Matrix(int m, int n, T val = 0) { init(m, n, val); };
+    Matrix<T>& operator=(const Matrix<T>& a) { copy(a); return *this; };
+    Matrix(const Matrix<T>& a) { copy(a); }
     ~Matrix() { clean(); };
 
 private:
@@ -82,7 +79,7 @@ private:
         }
     }
 
-    void copy(const Matrix<T> &a) {
+    void copy(const Matrix<T>& a) {
         clean();
         Rows = a.Rows;
         Cols = a.Cols;
@@ -110,29 +107,28 @@ public:
     friend Matrix<f_T1>& operator+=(Matrix<f_T1>&, const Matrix<f_T2>&);
 };
 
-// Placeholder for template static member
-template<typename T>
-T Matrix<T>::_default = 0;
-
 // common operations and helpers
 template <typename T>
-Matrix<T> Inverse(const Matrix<T> &a, bool& e) { return a.Inverse(e); }
+Matrix<T> Inverse(const Matrix<T>& a, bool& e) { return a.Inverse(e); }
 
 template <typename T>
-Matrix<T> Transpose(const Matrix<T> &a) { return a.Transpose(); }
+Matrix<T> Transpose(const Matrix<T>& a) { return a.Transpose(); }
 
 template <typename T>
-T Trace(const Matrix<T> &a, bool& e) { return a.Trace(e); }
+T Trace(const Matrix<T>& a, bool& e) { return a.Trace(e); }
+
+template <typename T>
+T Determinant(const Matrix<T>& a, bool& e) { return a.Determinant(e); }
 
 template <typename T>
 Matrix<T> Matrix<T>::Inverse(bool& error) const {
+    T temp;
     int n = Rows;
     Matrix<T> left(*this);
-    if (!(error = n != Cols)) {
-        Matrix<T> right(n, n, 0);
-        T temp;
-        T *r_temp = new T[n],
-          *l_temp = new T[n];
+
+    if (!(error = Rows != Cols)) {
+        Matrix<T> right(n, n);
+        std::vector<T> r_temp(n), l_temp(n);
 
         // Make right the Identity Matrix
         for (int i = 0; i < n; ++i) {
@@ -145,8 +141,6 @@ Matrix<T> Matrix<T>::Inverse(bool& error) const {
             while (left[pivot][col] == 0) {
                 if (++pivot >= n) {
                     error = true;
-                    delete[] r_temp;
-                    delete[] l_temp;
                     return left;
                 }
             }
@@ -189,15 +183,11 @@ Matrix<T> Matrix<T>::Inverse(bool& error) const {
             for (int j = 0; j < n; ++j) {
                 if (left[i][j] != (i == j ? 1 : 0)) {
                     error = true;
-                    delete[] r_temp;
-                    delete[] l_temp;
                     return left;
                 }
             }
         }
 
-        delete[] r_temp;
-        delete[] l_temp;
         return right;
     }
 
@@ -216,8 +206,20 @@ Matrix<T> Matrix<T>::Transpose() const {
 }
 
 template <typename T>
+T Matrix<T>::Determinant(bool& error) const {
+    T sum = 0;
+    int n = Rows;
+
+    if (!(error = Rows != Cols)) {
+        // Not yet implemented
+        error = true;
+    }
+    return sum;
+}
+
+template <typename T>
 T Matrix<T>::Trace(bool& error) const {
-    T sum = _default;
+    T sum = 0;
 
     if (!(error = Rows != Cols)) {
         for (int i = 0; i < Rows; ++i) {
@@ -230,7 +232,7 @@ T Matrix<T>::Trace(bool& error) const {
 
 // iostream handlers
 template <typename T>
-std::istream& operator>>(std::istream &in, Matrix<T> &obj) {
+std::istream& operator>>(std::istream& in, Matrix<T>& obj) {
     int m, n;
     in >> m;
     in >> n;
@@ -244,7 +246,7 @@ std::istream& operator>>(std::istream &in, Matrix<T> &obj) {
 }
 
 template <typename f_T>
-std::ostream& operator<<(std::ostream &out, const Matrix<f_T> &obj) {
+std::ostream& operator<<(std::ostream& out, const Matrix<f_T>& obj) {
     out << std::endl;
     for (int i = 0; i < obj.Rows; ++i) {
         out << "[";
@@ -261,7 +263,7 @@ std::ostream& operator<<(std::ostream &out, const Matrix<f_T> &obj) {
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream &out, const std::vector<T> &obj) {
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& obj) {
     out << "[";
     for (auto i = obj.begin(); i < obj.end(); ++i) {
         out << *i;
@@ -275,7 +277,7 @@ std::ostream& operator<<(std::ostream &out, const std::vector<T> &obj) {
 
 // matrix addition
 template <typename f_T1, typename f_T2>
-Matrix<f_T1>& operator+=(Matrix<f_T1> &a, const Matrix<f_T2> &b) {
+Matrix<f_T1>& operator+=(Matrix<f_T1>& a, const Matrix<f_T2>& b) {
     for (int i = 0; i < a.Rows; ++i) {
         for (int j = 0; j < a.Cols; ++j) {
             a._array[i][j] += b._array[i][j];
@@ -286,7 +288,7 @@ Matrix<f_T1>& operator+=(Matrix<f_T1> &a, const Matrix<f_T2> &b) {
 }
 
 template <typename T1, typename T2>
-Matrix<T1> operator+(const Matrix<T1> &a, const Matrix<T2> &b) {
+Matrix<T1> operator+(const Matrix<T1>& a, const Matrix<T2>& b) {
     Matrix<T1> c(a);
     c += b;
     return c;
@@ -294,7 +296,7 @@ Matrix<T1> operator+(const Matrix<T1> &a, const Matrix<T2> &b) {
 
 // matrix subtraction
 template <typename f_T1, typename f_T2>
-Matrix<f_T1>& operator-=(Matrix<f_T1> &a, const Matrix<f_T2> &b) {
+Matrix<f_T1>& operator-=(Matrix<f_T1>& a, const Matrix<f_T2>& b) {
     for (int i = 0; i < a.Rows; ++i) {
         for (int j = 0; j < a.Cols; ++j) {
             a._array[i][j] -= b._array[i][j];
@@ -305,7 +307,7 @@ Matrix<f_T1>& operator-=(Matrix<f_T1> &a, const Matrix<f_T2> &b) {
 }
 
 template <typename T1, typename T2>
-Matrix<T1> operator-(const Matrix<T1> &a, const Matrix<T2> &b) {
+Matrix<T1> operator-(const Matrix<T1>& a, const Matrix<T2>& b) {
     Matrix<T1> c(a);
     c -= b;
     return c;
@@ -313,10 +315,10 @@ Matrix<T1> operator-(const Matrix<T1> &a, const Matrix<T2> &b) {
 
 // vector multiplication
 template <typename f_T>
-std::vector<f_T> operator*(const Matrix<f_T> &a, const std::vector<f_T> &x) {
+std::vector<f_T> operator*(const Matrix<f_T>& a, const std::vector<f_T>& x) {
     std::vector<f_T> b(a.Rows);
     for (int i = 0; i < a.Rows; ++i) {
-        b[i] = a._default;
+        b[i] = 0;
         for (int j = 0; j < a.Cols; ++j) {
             b[i] += a._array[i][j] * x[j];
         }
@@ -326,7 +328,7 @@ std::vector<f_T> operator*(const Matrix<f_T> &a, const std::vector<f_T> &x) {
 
 // matrix multiplication
 template <typename T1, typename T2>
-Matrix<T1>& operator*=(Matrix<T1> &a, const Matrix<T2> &b) {
+Matrix<T1>& operator*=(Matrix<T1>& a, const Matrix<T2>& b) {
     // Matrix multiplication is more complicated than numeric addition
     // Because 'a' would need to be resized, it is more efficient
     // To use operator* here than to use '*=' in 'operator*'
@@ -334,8 +336,8 @@ Matrix<T1>& operator*=(Matrix<T1> &a, const Matrix<T2> &b) {
 }
 
 template <typename f_T1, typename f_T2>
-Matrix<f_T1> operator*(const Matrix<f_T1> &a, const Matrix<f_T2> &b) {
-    Matrix<f_T1> c(a.Rows, b.Cols, a._default);
+Matrix<f_T1> operator*(const Matrix<f_T1>& a, const Matrix<f_T2>& b) {
+    Matrix<f_T1> c(a.Rows, b.Cols, 0);
     for (int i = 0; i < a.Rows; ++i) {
         for (int j = 0; j < a.Cols; ++j) { // a.Cols = b.Rows (or error)
             for (int k = 0; k < b.Cols; ++k) {
@@ -349,7 +351,7 @@ Matrix<f_T1> operator*(const Matrix<f_T1> &a, const Matrix<f_T2> &b) {
 // scalar multiplication
 template <typename T, typename N,
     typename std::enable_if<std::is_arithmetic<N>::value>::type* = nullptr>
-Matrix<T> operator*(const N &c, const Matrix<T> &a) {
+Matrix<T> operator*(const N& c, const Matrix<T>& a) {
     Matrix<T> b(a);
     for (int i = 0; i < b.Rows; ++i) {
         for (int j = 0; j < b.Cols; ++j) {
@@ -361,7 +363,7 @@ Matrix<T> operator*(const N &c, const Matrix<T> &a) {
 
 template <typename T, typename N,
     typename std::enable_if<std::is_arithmetic<N>::value>::type* = nullptr>
-Matrix<T> operator*(const Matrix<T> &a, const N &c) { return c * a; }
+Matrix<T> operator*(const Matrix<T>& a, const N& c) { return c * a; }
 
 // complex scalar multiplication
 template <typename N1, typename C, typename N2, typename T,
@@ -369,7 +371,7 @@ template <typename N1, typename C, typename N2, typename T,
     typename std::enable_if<std::is_arithmetic<N2>::value>::type* = nullptr,
     typename std::enable_if<std::is_same<C, std::complex<N1>>::value>::type* = nullptr,
     typename std::enable_if<std::is_same<T, std::complex<N2>>::value>::type* = nullptr>
-Matrix<T> operator*(const C &c, const Matrix<T> &a) {
+Matrix<T> operator*(const C& c, const Matrix<T>& a) {
     Matrix<T> b(a);
     for (int i = 0; i < b.Rows; ++i) {
         for (int j = 0; j < b.Cols; ++j) {
@@ -384,4 +386,4 @@ template <typename N1, typename C, typename N2, typename T,
     typename std::enable_if<std::is_arithmetic<N2>::value>::type* = nullptr,
     typename std::enable_if<std::is_same<C, std::complex<N1>>::value>::type* = nullptr,
     typename std::enable_if<std::is_same<T, std::complex<N2>>::value>::type* = nullptr>
-Matrix<T> operator*(const Matrix<T> &a, const C &c) { return c * a; }
+Matrix<T> operator*(const Matrix<T>& a, const C& c) { return c * a; }
