d8e6f7bdc8bbb8ee591bc16d5f05c27d1a32b5c7
John Call <johnkc1992@gmail.com>
2015-04-14 15:28:38 -0600

    Finish problem 1

--- src/assign6/assign6.cpp
+++ src/assign6/assign6.cpp
@@ -30,20 +30,50 @@ Table RK4(ODE f, Point init, decimal t_final, double h) {
     return data;
 }
 
-void assign6::main() {
+void A6Prob1() {
+    Table exact{
+        {1950, 2555},
+        {1960, 3040},
+        {1970, 3708},
+        {1980, 4454},
+        {1990, 5276},
+        {2000, 6079},
+    };
+    Table actual{
+        {1950, 2555},
+        {1960, 3040},
+        {1970, 3708},
+        {1980, 4454},
+        {1990, 5276},
+        {2000, 6079},
+    };
+
     const double kgm = 0.026;
     const double pmax = 12000; // p measured in millions
     ODE dpdt = [&kgm, &pmax](const decimal& t, const double& p) {
         return kgm * (1 - p / pmax) * p;
     };
+
     Table results = RK4(dpdt, std::make_pair(1950, 2555), 2000, 0.1);
 
-    std::cout << std::left
-        << std::setw(10) << "t" << std::setw(10) << "p" << std::endl
-        << "--------------------" << std::endl;
+    std::cout << "Problem 1: " << std::left << std::endl
+        << std::setw(10) << "t"
+        << std::setw(10) << "p"
+        << std::setw(10) << "p_exact"
+        << std::setw(10) << "p_actual"
+        << std::endl
+        << "----------------------------------------" << std::endl;
     for (decimal year = 1950; year <= 2000; year += 10) {
         std::cout
             << std::setw(10) << (double)year
-            << std::setbase(10) << results[year] << std::endl;
+            << std::setw(10) << results[year]
+            << std::setw(10) << exact[year]
+            << std::setw(10) << actual[year]
+            << std::endl;
     }
+    std::cout << std::endl;
+}
+
+void assign6::main() {
+    A6Prob1();
 }
